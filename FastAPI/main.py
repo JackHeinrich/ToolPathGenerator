8657645c52ed2420550c87f1875909e84f2447f3
JackHeinrich <JackHeinrich2005@gmail.com>
2023-05-10 18:25:37 -0400

    added serpapi to tools

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -5,20 +5,6 @@ from pydantic import BaseModel
 
 import ConversationalDocumentSearchingTool as DocSearcher
 
-import tkinter as tk
-from tkinter import filedialog
-
-#FILE PROMPT SETUP
-root = tk.Tk()
-root.withdraw()
-
-app = FastAPI(docs_url='/docs1')
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-
 @app.get("/llm")
 def get_llm_respose(query: str):
     response = DocSearcher.AskLLM(query)
